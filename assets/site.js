36471673bb5f399acc9208871298ab3223bc8e45
5rnhcymzrw-sys <5rnhcymzrw@privaterelay.appleid.com>
2026-08-12 19:02:50 +0200

    Bump global stylesheet version

--- assets/site.js
+++ assets/site.js
@@ -6,7 +6,7 @@
   if (!document.querySelector('link[href*="assets/global.css"]')) {
     const globalStyle = document.createElement('link');
     globalStyle.rel = 'stylesheet';
-    globalStyle.href = `${basePath}assets/global.css?v=20260812-6`;
+    globalStyle.href = `${basePath}assets/global.css?v=20260812-7`;
     document.head.appendChild(globalStyle);
   }
 
